4fec91c09dffa59ee432a9daab8e1142bc0eb7fd
Johndelsin <johndelsin22@gmail.com>
2022-10-13 15:26:47 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stduo.h>
+#include <stdio.h>
+
 /**
- * main -Entry
+ * main - Printing
+ *
  * Return: Always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
