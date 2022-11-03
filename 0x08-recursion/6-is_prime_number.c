f1045f76dc50e667ed2e5db4fdae508207716a92
Johndelsin <johndelsin22@gmail.com>
2022-11-03 07:12:37 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,15 +13,15 @@ int is_prime_number(int n)
  * check_prime - it checks prime numbers
  * @n: number
  * @i: iterator
- * Return: a l or 0
+ * Return: a 1 or 0
  */
 int check_prime(int n, int i)
 {
 	if (n <= 1)
 		return (0);
 	if (n % i == 0 && i > 1)
-		return (0)
+		return (0);
 			if ((n / i) < i)
-				print (1);
+				return (1);
 	return (check_prime(n, i + 1));
 }
