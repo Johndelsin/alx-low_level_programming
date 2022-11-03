fc98b6b9a928fdda210cbb6d5e63a151bde4a44e
Johndelsin <johndelsin22@gmail.com>
2022-11-02 23:59:04 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,14 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: pointer block of memory to fill
- * Return: void
+ * @s: string s
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s) /*If statement*/
-	{
-	_print_rev_recursion(s + 1); /*add s*/
-	_putchar(*s); /*prints s*/
-	}
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
