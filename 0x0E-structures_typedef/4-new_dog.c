05e032250587344451624e02a65a8ab3f262ff75
Johndelsin <johndelsin22@gmail.com>
2022-11-15 09:39:40 -0800

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(char *str)
 {
 	int len = 0;
+
 	while (*str++)
 		len++;
 	return (len);
@@ -28,6 +29,7 @@ int _strlen(char *str)
 char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
+
 	for (index = 0; src[index]; index++)
 		dest[index] = src[index];
 	dest[index] = '\0';
