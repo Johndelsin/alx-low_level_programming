8e5e08664c676917378da32279707fc17c3ed61a
Johndelsin <johndelsin22@gmail.com>
2023-01-16 23:58:56 -0800

    task 0

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills the memeory with a constant byte
+ * @s: A pointer to the memory area to be filled
+ * @b: The character to fill the memory area with
+ * @n: The number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory area @s
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
